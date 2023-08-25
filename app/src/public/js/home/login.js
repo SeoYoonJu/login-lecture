9a285e993d4167a3f06daf7326848a2bb443a04d
grapyju <yoonju203@naver.com>
2023-08-25 20:58:21 +0900

    dom_html

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,2 +1,15 @@
-console.log("hello");
-console.log("bye")
+"use strict";
+
+const id = document.querySelector("#id"), //태그에 id로 부여되어 있는 id를 가져와달라
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("#button");
+
+loginBtn.addEventListener("click",login);
+
+function login(){
+    const req = {
+        id : id.value,  //.value => id의 입력값을 받아옴
+        psword : psword.value,
+    };
+
+}
